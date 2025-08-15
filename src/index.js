a485f460384f4fe85971592808a66d707a34f0e0
Arindam roy <arindam_ug_23@ei.nits.ac.in>
2025-08-15 21:08:41 +0530

    feat: add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+// import express from 'express';
+import { app } from './app.js';
+
+// const app = express();
 
 dotenv.config({
     path: './env'
 })
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+    console.log("Connected to MongoDB");
+    app.on('error', (err) => {
+        console.error('Error in Express app:', err);
+    });
+})
+.catch(error => {
+    console.error("Failed to connect to MongoDB:", error);
+    process.exit(1); // Exit process with failure
+});
 
 /*
 import express from 'express';
