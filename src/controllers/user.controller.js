98fa110d8539cb080b354c34c837e8203a447d46
Arindam roy <arindam_ug_23@ei.nits.ac.in>
2025-09-09 03:11:04 +0530

    feat: enhance user registration by adding image upload functionality and validation checks

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,36 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
-
+import { ApiError } from '../utils/ApiError.js';
+import { User } from '../models/user.model.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     // Registration logic here
-    res.status(201).json(
-        { 
-            message: 'User registered successfully' 
+    
+    // get details from user
+    //validate details
+    // check if user already exists
+    // check for images , check for avatar
+    // upload to cloudinary,avatar
+    // create user object - create entry in db
+    // remove password from response and refresh token
+    //check for user creation
+    // return result
 
-        }
-    );
+    const { fullName , email , password , username } = req.body;
+    console.log(req.body);
+
+    if(
+        [fullName , email , password , username].some(field => field?.trim()==="")
+    ) {
+        throw new ApiError(400,"All fields are required");
+    }
+
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if(existingUser){
+        throw new ApiError(409,"User with this email or username already exists");
+    }
+
+
+
 });
 
 export { registerUser };
